4db6994b48695e43e9ec4b17c72d313027041eed
arlo <arloherrine@gmail.com>
2015-12-22 02:58:10 +0000

    fixing things up

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ class AnswerpTCPHandler(SocketServer.StreamRequestHandler):
     """
     def __init__(self):
         self.server.data = {}
-	# This is just for testing
-	self.server.data = {'calls': [
-			{'time': '01:31', 'name': 'Jeff', 'callNumber': '610-777-3340'},
-			{'time': '11:28', 'name': 'Mary', 'callNumber': '512-345-8750'},
-		],
-		'texts': [
-			{'date': '03:52', 'name': 'Mimi Bonney', 'body': 'Hey there, what is up?'},
-			{'date': '08:17', 'name': 'Steven F. Stephenson', 'body': 'This is a very long text from your friend steve who has a long stupid name and sends long texts'},
-		]}
 
 
     def handle(self):
@@ -55,12 +46,12 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
 
 class LCDUI:
-    def __init__(self, *args, **keywords):
-        self.state = 'off'
+    def __init__(self, server, *args, **keywords):
+	self.server = server
         self.lcd = LCD.Adafruit_CharLCDPlate()
         self.top_selected = True
-        self.content = ['','']
         self.index = 0
+	self.turn_off()
         self.callbacks = {}
         self.timers = {
                 LCD.UP: 0,    
@@ -83,26 +74,34 @@ class LCDUI:
         self.register_callback('main_menu', LCD.UP, False, self.scroll_up)
         self.register_callback('main_menu', LCD.DOWN, False, self.scroll_down)
         self.register_callback('main_menu', LCD.SELECT, False, self.main_menu_select)
+        self.register_callback('main_menu', LCD.RIGHT, False, self.main_menu_select)
 
         self.register_callback('call_menu', LCD.UP, False, self.scroll_up)
         self.register_callback('call_menu', LCD.DOWN, False, self.scroll_down)
         self.register_callback('call_menu', LCD.SELECT, False, self.call_menu_select)
+        self.register_callback('call_menu', LCD.RIGHT, False, self.call_menu_select)
+        self.register_callback('call_menu', LCD.LEFT, False, self.turn_on)
 
         self.register_callback('text_menu', LCD.UP, False, self.scroll_up)
         self.register_callback('text_menu', LCD.DOWN, False, self.scroll_down)
         self.register_callback('text_menu', LCD.SELECT, False, self.text_menu_select)
+        self.register_callback('text_menu', LCD.RIGHT, False, self.text_menu_select)
+        self.register_callback('text_menu', LCD.LEFT, False, self.turn_on)
 
         self.register_callback('call_display', LCD.UP, False, self.scroll_up)
         self.register_callback('call_display', LCD.DOWN, False, self.scroll_down)
         self.register_callback('call_display', LCD.SELECT, False, self.open_call_menu)
+        self.register_callback('call_display', LCD.LEFT, False, self.open_call_menu)
 
         self.register_callback('text_display', LCD.UP, False, self.scroll_up)
         self.register_callback('text_display', LCD.DOWN, False, self.scroll_down)
         self.register_callback('text_display', LCD.SELECT, False, self.open_text_menu)
+        self.register_callback('text_display', LCD.LEFT, False, self.open_text_menu)
 
     def register_callback(self, state, button, long, func):
-        self.callbacks.setdefault((button, long), [])
-	self.callbacks.get((button, long)).append(func)
+	self.callbacks[(state, button, long)] = func
+        #self.callbacks.setdefault((button, long), [])
+	#self.callbacks.get((button, long)).append(func)
 
     def display(self):
         if self.top_selected:
@@ -137,7 +136,7 @@ class LCDUI:
         self.lcd.set_backlight(0)
         self.lcd.clear()
         self.state = 'off'
-        self.context = ['','']
+        self.content = ['','']
 
     def scroll_up(self):
         self.top_selected = True
@@ -148,7 +147,10 @@ class LCDUI:
     def scroll_down(self):
         self.top_selected = not self.selectable
         self.index += 1
-        if self.index >= len(self.content):
+        maximum = len(self.content)
+        if not self.selectable:
+            maximum -= 1
+        if self.index >= maximum:
             self.index -= 1
         self.display()
 
@@ -171,16 +173,16 @@ class LCDUI:
         self.index = 0
         self.top_selected = True
         self.selectable = True
-        self.content = ["{}-{}".format(call['time'], call['name']) for call in self.server.data['texts']]
+        self.content = ["{}-{}".format(call['time'], call['name']) for call in self.server.data['calls']]
         self.display()
 
     def text_menu_select(self):
-        text = self.server.data['text'][self.index]
+        text = self.server.data['texts'][self.index]
         self.state = 'text_display'
         self.index = 0
         self.top_selected = True
         self.selectable = False
-        self.content = [text['body'][i:i+16] for i in range(0, len(text['body]']), 16)]
+        self.content = [text['body'][i:i+16] for i in range(0, len(text['body']), 16)]
         self.display()
 
     def call_menu_select(self):
@@ -196,28 +198,46 @@ class LCDUI:
             ]
         self.display()
 
-    def main_loop(server):
+    def main_loop(self):
         while(True):
             for key in self.timers:
                 self.handle_key(key)
             time.sleep(0.1)
 
-    def handle_key(key):
+    def handle_key(self, key):
         if self.lcd.is_pressed(key):
             self.timers[key] += 1
             if self.timers[key] == 10:
-                self.callbacks[(key, True)]()
-        elif self.timers[key]:
+	        if (self.state, key, True) in self.callbacks:
+		    self.callbacks[(self.state, key, True)]()
+                #for callback in self.callbacks[(key, True)]:
+		#	callback()
+        else:
+	    old_count = self.timers[key]
             self.timers[key] = 0
-            self.callbacks[(key, False)]()
+	    if old_count and not old_count >= 10 and (self.state, key, False) in self.callbacks:
+	        self.callbacks[(self.state, key, False)]()
+            #for callback in self.callbacks[(key, False)]:
+	#		callback()
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8888
 
     server = ThreadedTCPServer((HOST, PORT), AnswerpTCPHandler)
+    server.data = {'calls': [
+		{'time': '01:31', 'name': 'jeff', 'callNumber': '610-777-3340'},
+		{'time': '11:28', 'name': 'mary', 'callNumber': '512-345-8750'},
+		{'time': '04:43', 'name': 'Jim the baker', 'callNumber': '512-346-2946'},
+	],
+	'texts': [
+		{'date': '03:52', 'name': 'mimi bonney', 'body': 'hey there, what is up?'},
+		{'date': '08:17', 'name': 'steven f. stephenson', 'body': 'this is a very long text from your friend steve who has a long stupid name and sends long texts'},
+		{'date': '06:24', 'name': 'steven f. stephenson', 'body': 'This is a shorter text from stevie'},
+		{'date': '07:18', 'name': 'mimi bonney', 'body': 'another mimi text to read that is a bit longer so is a cople lines'},
+	]}
     server_thread = threading.Thread(name='data-update-server-thread', target=server.serve_forever)
 
-    ui = LCDUI()
+    ui = LCDUI(server)
     ui.main_loop()
 
